5ec68bc33b846ad5192d256e7fd382c851d0dcbf
wang hao <haozit146@gmail.com>
2016-06-23 00:53:21 +0800

    log context

--- modules/cordova-plugin-crosswalk-webview/hooks/before_build/000-build_64_bit.js
+++ modules/cordova-plugin-crosswalk-webview/hooks/before_build/000-build_64_bit.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 module.exports = function(context) {
+  console.log('contxt :', context);
   var fs = require('fs');
   var path = require('path');
   var paths = {
@@ -18,14 +19,14 @@ module.exports = function(context) {
   var libPath = path.join(rootPath, 'org.xwalk/xwalk_core_library/17.46.448.10/cd626c32360d9b48b60cf685b38fa7f5b31156df');
   var tailPath = 'org.xwalk/xwalk_core_library/17.46.448.10/cd626c32360d9b48b60cf685b38fa7f5b31156df/xwalk_core_library-17.46.448.10.aar';
   var xwalkPath = path.join(rootPath, tailPath);
-  var platform = context.cmdLine.indexOf('--x86')>-1?'x86':'arm';
-  var channel = context.opts.options.indexOf('--dev')>-1?'dev':'release';
-  var cloudPath = paths[channel][platform];
+  // var platform = context.cmdLine.indexOf('--x86')>-1?'x86':'arm';
+  // var channel = context.opts.options.indexOf('--dev')>-1?'dev':'release';
+  // var cloudPath = paths[channel][platform];
   var deferral = context.requireCordovaModule('q').defer(),
     UpdateConfig = require('./../update_config.js'),
     updateConfig = new UpdateConfig(context);
 
-  console.log('befor build >>>>>> :', cloudPath);
+  console.log('befor build >>>>>> :');
   console.log(rootPath, fs.existsSync(rootPath));
   console.log(libPath, fs.existsSync(libPath));
   deferral.resolve();
