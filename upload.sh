08751b0841e3dcff3c25650cce764b05fa9e149e
wang hao <haozit146@gmail.com>
2016-06-24 02:08:34 +0800

    file rename

--- upload.sh
+++ upload.sh
@@ -4,6 +4,7 @@ qshell account $access_key $secret_key
 for localPath in `find ./dist -name "*[arm|x86].apk" -maxdepth 1`; do
   echo $localPath
   filename=${localPath/.apk/"_"`date "+%Y-%m-%d %H:%M:%S"`".apk"};
+  filename=${filename/' '/"_"};
   filename=${filename##*/};
   echo $filename
   qshell rput $bucket $filename $localPath true
