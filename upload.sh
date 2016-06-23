2d124e675ae116355dc8aa4a198a3aa8bcbdb5b9
wang hao <haozit146@gmail.com>
2016-06-24 01:51:18 +0800

    file rename

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
 set -e
 source ./qiniu.conf
 qshell account $access_key $secret_key
-apks = `find ./dist -name "*[arm|x86].apk" -maxdepth 1`
-for localName in $apks; do
-  echo $localName
-  filename = ${localName/.apk/`date "+%Y-%m-%d %H:%M:%S"`".apk"};
+for localPath in `find ./dist -name "*[arm|x86].apk" -maxdepth 1`; do
+  echo $localPath
+  filename=${localPath/.apk/"_"`date "+%Y-%m-%d %H:%M:%S"`".apk"};
+  filename=${filename##*/};
   echo $filename
-  qshell rput $bucket $filename $localName true
+  qshell rput $bucket $filename $localPath true
 done
