a7fbff6c97ac409e0582090dc710b6a77890eae7
wang hao <haozit146@gmail.com>
2016-06-24 00:43:15 +0800

    remove letter space

--- upload.sh
+++ upload.sh
@@ -1,9 +1,10 @@
 set -e
 source ./qiniu.conf
-echo "access_key: "$access_key
 qshell account $access_key $secret_key
 apks = `find ./dist -name "*[arm|x86].apk" -maxdepth 1`
 for localName in $apks; do
+  echo $localName
   filename = ${localName/.apk/`date "+%Y-%m-%d %H:%M:%S"`".apk"};
+  echo $filename
   qshell rput $bucket $filename $localName true
 done
