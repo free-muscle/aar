d060618663d7812e4462e0e6e10ac024e840cf47
wang hao <haozit146@gmail.com>
2017-10-09 22:28:18 +0800

    修复qshell

--- install-qshell.sh
+++ install-qshell.sh
@@ -1,5 +1,6 @@
 mkdir qiniu
-wget -O ./qiniu/qshell-linux-x64 https://dn-devtools.qbox.me/2.1.5/qshell-linux-x64?ref=developer.qiniu.com
+wget -O ./qiniu/qshell https://dn-devtools.qbox.me/2.1.5/qshell-linux-x64?ref=developer.qiniu.com
 # md5sum /tmp/qshell.zip
 # unzip /tmp/qshell.zip -d ./qiniu || exit 2
-mv ./qiniu/qshell-linux-x64 ./qiniu/qshell
+# mv ./qiniu/qshell-linux-x64 ./qiniu/qshell
+chmod +x ./qiniu/qshell
