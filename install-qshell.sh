c8ee8ed8b1f67a618e9cfdf268b6948ea24b7f1c
wang hao <haozit146@gmail.com>
2017-10-09 20:55:28 +0800

    修复qshell地址

--- install-qshell.sh
+++ install-qshell.sh
@@ -1,4 +1,4 @@
-wget -O /tmp/qshell.zip http://devtools.qiniu.com/qshell-v1.8.0.zip
+wget -O /tmp/qshell.zip https://dn-devtools.qbox.me/2.1.5/qshell-linux-x64?ref=developer.qiniu.com
 md5sum /tmp/qshell.zip
 unzip /tmp/qshell.zip -d ./qiniu || exit 2
 mv ./qiniu/qshell_linux_amd64 ./qiniu/qshell
