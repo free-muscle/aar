b23a191b2aaa3b7c994ceb72e79d87858d92a768
wang hao <haozit146@gmail.com>
2017-10-24 14:06:17 +0800

    specify cordova version

--- install-qshell.sh
+++ install-qshell.sh
@@ -1,6 +1,3 @@
 mkdir qiniu
 wget -O ./qiniu/qshell https://dn-devtools.qbox.me/2.1.5/qshell-linux-x64?ref=developer.qiniu.com
-# md5sum /tmp/qshell.zip
-# unzip /tmp/qshell.zip -d ./qiniu || exit 2
-# mv ./qiniu/qshell-linux-x64 ./qiniu/qshell
 chmod +x ./qiniu/qshell
